9c451cdf65b89507e3f2f8ac8346faf88b37e7f0
Fabricio Borgobello <fborgobello@gmail.com>
2021-02-02 14:49:10 -0300

    Se añadió NavBar, config de estilos, CartWidget e ItemListContainer

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import CartWidget from "./CartWidget";
+import { navbar, logo, options } from "./NavBar.module.scss";
+
+const NavBar = () => {
+  return (
+    <nav className={navbar}>
+      <div className={logo}>logo</div>
+      <ul className={options}>
+        <li>
+          <a href="">Contacto</a>
+        </li>
+        <li>
+          <a href="">Categorias</a>
+        </li>
+        <li>
+          <CartWidget />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
